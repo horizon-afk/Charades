df3ae9e049041b64037054069d4ef160d308cceb
Umesh <umeshsarkar19@outlook.com>
2024-10-19 22:46:49 +0530

    Fined tuned the composables

--- app/src/main/java/com/example/charades/MainActivity.kt
+++ app/src/main/java/com/example/charades/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -13,18 +15,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.Icon
-import androidx.compose.material3.LocalTextStyle
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -32,10 +32,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.charades.ui.theme.CharadesTheme
@@ -47,17 +47,27 @@ class MainActivity : ComponentActivity() {
         setContent {
             CharadesTheme {
                 Scaffold {innerPadding->
-                    Column(modifier = Modifier.padding(innerPadding)) {
-                        Box {
+                    Column(
+                        modifier = Modifier.padding(innerPadding).fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                        ) {
+                        Box(modifier = Modifier.padding(15.dp)) {
                             Text(text = "Enter text")
                         }
                         Box (
-                            modifier = Modifier.height(200.dp),
+                            modifier = Modifier.height(200.dp).width(300.dp)
+                                .border(2.dp, Color(0xFF2196F3), shape = RoundedCornerShape(12.dp)),
                             contentAlignment = Alignment.Center
                         ){
                             var name by remember { mutableStateOf(TextFieldValue("")) }
-                            BasicTextField(
-                                textStyle = TextStyle.Default.copy(textAlign = TextAlign.Center, fontSize = 28.sp),
+                           BasicTextField(
+                               cursorBrush = SolidColor(LocalContentColor.current),
+                                textStyle = TextStyle.Default.copy(
+                                    textAlign = TextAlign.Center,
+                                    fontSize = 28.sp,
+                                    color = LocalContentColor.current
+                                ),
                                 value = name,
                                 onValueChange = {
                                     val maxLength = 80
@@ -66,10 +76,10 @@ class MainActivity : ComponentActivity() {
                             )
 
                         }
-                        Box {
+                        Box(modifier = Modifier.padding(12.dp)) {
                             Button(
                                 shape = CircleShape,
-                                modifier = Modifier.size(50.dp),
+                                modifier = Modifier.size(75.dp),
                                 colors = buttonColors(Color(0xFF2196F3)),
                                 contentPadding = PaddingValues(0.dp),
                                 onClick = {}
@@ -77,7 +87,7 @@ class MainActivity : ComponentActivity() {
                             ) {
                                 Icon(Icons.Default.PlayArrow,
                                     contentDescription = "",
-                                    modifier = Modifier.size(24.dp),
+                                    modifier = Modifier.size(50.dp),
                                     tint = Color(0xFFFFFFFF)
                                 )
 
